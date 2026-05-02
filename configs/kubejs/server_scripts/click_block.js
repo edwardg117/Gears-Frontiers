12fd1bcbfe71f516071139f0882b38f541088236
edwardg117 <8350193+edwardg117@users.noreply.github.com>
2026-05-02 16:27:53 +0800

    Update scripts and quests - Fix encasing enchantable machinery. - Add a bunch of quests for the new features.

---
+++ configs/kubejs/server_scripts/click_block.js
@@ -0,0 +1,26 @@
+// Stolen from ChosenArchitect https://www.curseforge.com/minecraft/modpacks/chosens-create-integrated
+BlockEvents.rightClicked(event => {
+    const { block, item } = event
+    
+    if (block.id.startsWith('createenchantablemachinery:')) {
+        if (item.hasTag('c:stones') || 
+            item.hasTag('minecraft:stone_tool_materials') || 
+            item.id === 'minecraft:stone') {
+            
+            event.cancel()
+            return 
+        }
+    }
+
+    const targetMachines = [
+        'createenchantablemachinery:enchantable_mechanical_press',
+        'createenchantablemachinery:enchantable_mechanical_mixer'
+    ]
+
+    if (targetMachines.includes(block.id)) {
+        if (item.hasTag('create:casing')) {
+
+            event.cancel()
+        }
+    }
+})
